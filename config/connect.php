67c9e7d81f7bccd6ff6beeb67ca22b3c919ef0e7
huicochea <cibercafehuico@gmail.com>
2016-03-15 17:04:05 -0600

    Se modifico la conexion a base de datos

--- config/connect.php
+++ config/connect.php
@@ -1,21 +1,6 @@
 <?php
 include("config.php");
-//$conn = mssql_connect($host_db,$usuario_db,$password_db);
 
+$conn = new PDO('mysql:host="$host_db";dbname="$nombre_db"', $usuario_db, $password_db);
 
-$conn = new PDO('mysql:host=$host_db;dbname=$nombre_db', $usuario_db, $password_db);
-
-/*
-$conn = mysql_connect($host_db,$usuario_db,$password_db);
-
-if(!($conn)){
-    echo "<p>Connection to mysql via mssql failed: ".$conn;
-    print_r($conn);
-    echo "</p>\n";
-}else{
-    //mssql_select_db($nombre_db, $conn);
-    mysql_select_db($nombre_db,$conn);
-
-}
-*/
 ?>
