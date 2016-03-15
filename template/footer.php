b9cc87dc99623e8aca1d86dd42d759b2f4ba2842
huicochea <cibercafehuico@gmail.com>
2016-03-15 16:58:57 -0600

    Se modifico la fecha

--- template/footer.php
+++ template/footer.php
@@ -33,7 +33,11 @@
 
   <div class="footer">
     <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="170" height="49"/> </div>
-    <div class="center_footer"> Huicosoft. All Rights Reserved <?php echo date("Y") ?><br />
+    <div class="center_footer"> Huicosoft. All Rights Reserved 
+    <?php 
+      date_default_timezone_set('America/Mexico_City');
+      echo date("Y") 
+    ?><br />
       <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" border="0" /></a><br />
       <img src="images/payment.gif" alt="" /> </div>
     <div class="right_footer"> <a href="#">Inicio</a> <a href="#">Ayuda</a> <a href="#">Movil</a> <a href="#">rss</a> <a href="contact.html">Contáctanos</a> </div>
