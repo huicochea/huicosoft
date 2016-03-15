50158d0c289259d10eba8bc53d89f7dde5f25923
huicochea <cibercafehuico@gmail.com>
2016-03-15 17:07:45 -0600

    se cambios host de base de datos por localhost

--- config/connect.php
+++ config/connect.php
@@ -1,6 +1,6 @@
 <?php
 include("config.php");
 
-$conn = new PDO('mysql:host="$host_db";dbname="$nombre_db"', $usuario_db, $password_db);
+$conn = new PDO('mysql:host="localhost";dbname="$nombre_db"', $usuario_db, $password_db);
 
 ?>
