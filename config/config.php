b90f6ec95cb1ee7422335b6f5871339674d6c540
Eduardo Castellanos Huicochea <cibercafehuico@gmail.com>
2017-04-12 16:13:20 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-	$usuario_db="dbo618110615";
-	$host_db='db618110615.db.1and1.com';
-	$password_db="nomelase";
-	$nombre_db="db618110615";
+	$usuario_db="";
+	$host_db='';
+	$password_db="";
+	$nombre_db="";
 ?>
